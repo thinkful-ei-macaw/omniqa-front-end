9ccafe5c6d017443aa92ea95f877f06ebe7937f9
dasha akhten <dasha.akhten@gmail.com>
2020-05-27 11:15:04 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>awesome team is awesome</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
