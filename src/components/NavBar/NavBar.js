2d1872338591505e34e489fcacd8ced0792a5946
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-05-27 16:16:07 -0600

    Reformatted NavBar.css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 export class NavBar extends Component {
   render() {
     return (
-      <nav className="nav">
+      <nav className='nav'>
         <ul>
+          <li id='logo'>
+            <img src={require('./logo.png')} alt='omni logo' />
+          </li>
           <li>
             <form>
-              <label>Search for questions</label>
-              <input type="text" />
-              <button type="submit">Find</button>
+              <input type='text' placeholder='Search for questions...' />
+              <button type='submit'>Find</button>
             </form>
           </li>
-          <li id="question-btn">
-            <Link to="/Question">Ask a question</Link>
+          <li>
+            <span>Q&A Home</span>
+          </li>
+          <li>
+            <span>Answer Q's</span>
+          </li>
+          <li id='question-btn'>
+            <Link to='/Question'>Ask a question</Link>
             {/* <button onClick="window.location.href='question.html'">Ask a question</button> */}
           </li>
         </ul>
