7cd3e64dfae9fe9b1a8b94fd8bf1dedab86b55b9
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-05-28 15:15:57 -0600

    Pushing for test

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
-import "./Registration.css";
-import { Link } from "react-router-dom";
-import AuthApiService from "../../Services/auth-api-service";
+import React, { Component } from 'react';
+import './Registration.css';
+import { Link } from 'react-router-dom';
+import AuthApiService from '../../Services/auth-api-service';
 
 export class Registration extends Component {
   static defaultProps = {
-    onRegistrationSuccess: () => {},
+    onRegistrationSuccess: () => {}
   };
   state = { error: null };
   handleSubmit = (ev) => {
     ev.preventDefault();
-    const name = ev.target[0];
-    const username = ev.target[1];
-    const password = ev.target[2];
+    const { name, username, password } = ev.target;
 
     this.setState({ error: null });
     AuthApiService.postUser({
       username: username.value,
       password: password.value,
-      name: name.value,
+      name: name.value
     })
       .then((user) => {
-        name.value = "";
-        username.value = "";
-        password.value = "";
+        name.value = '';
+        username.value = '';
+        password.value = '';
         this.props.onRegistrationSuccess(user);
       })
       .catch((res) => {
@@ -32,52 +30,38 @@ export class Registration extends Component {
   };
   render() {
     return (
-      <div className="Registration">
-        <form id="signup" onSubmit={this.handleSubmit}>
+      <div className='Registration'>
+        <form id='signup' onSubmit={this.handleSubmit}>
           <fieldset>
             <legend>sign up here</legend>
-            <div class="form-group">
-              <label for="username">Username</label>
-              <input
-                class="form-control"
-                type="text"
-                name="username"
-                id="username"
-                placeholder="james.bond"
-                required
-              />
+            <div class='form-group'>
+              <label for='username'>Username</label>
+              <input class='form-control' type='text' name='username' id='username' placeholder='james.bond' required />
             </div>
-            <div class="form-group">
-              <label for="name">Your name</label>
-              <input
-                class="form-control"
-                type="text"
-                name="name"
-                id="name"
-                placeholder="James Bond"
-                required
-              />
+            <div class='form-group'>
+              <label for='name'>Your name</label>
+              <input class='form-control' type='text' name='name' id='name' placeholder='James Bond' required />
             </div>
-            <div class="form-group">
-              <label for="password">Password</label>
+            <div class='form-group'>
+              <label for='password'>Password</label>
               <input
-                class="form-control"
-                type="password"
-                name="password"
-                id="password"
-                placeholder="********"
+                class='form-control'
+                type='password'
+                name='password'
+                id='password'
+                placeholder='********'
                 required
               />
             </div>
 
-            <div class="submit--reg">
-              <button class="submit-form" type="submit">
+            <div class='submit--reg'>
+              <button class='submit-form' type='submit'>
                 Register
               </button>
 
-              <Link to="/login">
-                {" "}
-                <button type="button">I am already a member</button>
+              <Link to='/login'>
+                {' '}
+                <button type='button'>I am already a member</button>
               </Link>
             </div>
           </fieldset>
