87a2758b28716b1759164d7fa273b979ddb99681
dasha akhten <dasha.akhten@gmail.com>
2020-05-27 19:44:35 -0700

    dashboard html

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,7 +7,55 @@ export class Dashboard extends Component {
     return (
       <div className="dashboard">
         <NavBar />
-        Dashboard
+        <section>
+          <div>
+            <p class="questions">What is the phone number for 911?</p>
+          </div>
+          <div class="image-info">
+            <div>
+              <p>Asked By</p>
+              <img src="Spongebob_squarepants.png" />
+            </div>
+          </div>
+          <div class="image-stats">
+            <p>Spongebob Squarepants</p>
+            <p>Lord of the Sea</p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <p class="questions">
+              If you can buy a dozen donuts for 12 cents, how many donuts can
+              you buy for $1.00?
+            </p>
+          </div>
+          <div class="image-info">
+            <div>
+              <p>Asked By</p>
+              <img src="Spongebob_squarepants.png" />
+            </div>
+            <div class="image-stats">
+              <p>Spongebob Squarepants</p>
+              <p>Lord of the Sea</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <p class="questions">Durrrrrr can't think of anything</p>
+          </div>
+          <div class="image-info">
+            <div>
+              <p>Asked By</p>
+              <img src="Spongebob_squarepants.png" />
+            </div>
+            <div class="image-stats">
+              <p>Spongebob Squarepants</p>
+              <p>Lord of the Sea</p>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
