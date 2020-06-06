89b39520417962737422bf4c5687461e5c8f813a
Alex Larrabee <nekotree556@gmail.com>
2020-06-06 11:02:20 -0700

    working on front end

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  // API_ENDPOINT: "http://localhost:8000",
   API_ENDPOINT: process.env.REACT_APP_API_URL || "http://localhost:8000",
   TOKEN_KEY: "omniqa-client-auth-token",
 };
