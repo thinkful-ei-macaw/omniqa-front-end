7fb36fff4caf8086623adc1283b24f89fdf563cb
Alex Larrabee <nekotree556@gmail.com>
2020-06-09 13:55:51 -0700

    answer route is posting to database

--- src/components/PostAnswer/postAnswer.js
+++ src/components/PostAnswer/postAnswer.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react';
 import AnswerApiService from '../../Services/answers-service';
-import AnswerContext from '../../Context/AnswerContext';
-import QuestionsApiService from '../../Services/questions-service';
+import { Link } from 'react-router-dom'
 
 
 
 export default class postAnswer extends Component {
-    static contextType = AnswerContext;
-    constructor(props) {
-        super(props);
-        this.state = {
-            questions: []
-        }
-    }
 
-    componentDidMount() {
-        this.context.clearError();
 
-        QuestionsApiService.getQuestions()
-            .then(question => {
-                console.log(question)
-                this.setState({
-                    questions: question
-                })
-            })
-            .catch(this.context.setError);
+
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log('Successful answer submit')
+
+        AnswerApiService.postAnswer(e.target.answer_body.value, this.props.match.params.question_id)
+
     }
 
     render() {
 
-
-        
         return (
-            <div>
-                See me
+            <div className="Answer">
+                <form className="answer-form" onSubmit={e => this.handleSubmit(e)}>
+                    <fieldset>
+                        <br />
+                        <label htmlFor="answer-label">Answer</label>
+                        <input
+                            className="answer-input"
+                            type="text"
+                            name="answer_body"
+                            id="answer-label"
+                            placeholder="type your answer here"
+                        />
+                        <br />
+                        <button type="submit">Submit Answer</button>
+
+                    </fieldset>
+                </form>
+                <Link to='/Dashboard'>Return to Dashboard</Link>
             </div>
         )
     }
