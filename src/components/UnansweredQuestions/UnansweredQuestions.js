8e9fce21da881f6e365a0aa07c1bc1c5847dd7d2
Connor McCormack <mccormack.connor.m@gmail.com>
2020-06-10 11:05:54 -0500

    refactor: deleted unused UnsweredQuestions file, refactored Unanswered Questions

--- src/components/UnansweredQuestions/UnansweredQuestions.js
+++ src/components/UnansweredQuestions/UnansweredQuestions.js
@@ -7,59 +7,28 @@ import QuestionsApiService from "../../Services/questions-service";
 import Moment from "react-moment";
 import Answer from "../Answer/Answer";
 import AnswersApiService from '../../Services/answers-service';
+import QuestionList from '../QuestionList/QuestionList';
 import { Link } from 'react-router-dom';
+import Sort from "../Sort/Sort";
+import TokenService from '../../Services/TokenService'
 
 export class UnansweredQuestions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions: [],
-    }
-  }
 
   static contextType = QuestionContext;
 
-  componentDidMount() {
-    this.context.clearError();
-
-    QuestionsApiService.getQuestions()
-      .then(question => {
-        console.log(question)
-        this.setState({
-          questions: question
-        })
-      })
-      .catch(this.context.setError);
-  }
-
   render() {
-    const questions = this.state.questions;
+    const questions = this.context.questions;
+    const { user_id } = TokenService.readJwtToken()
 
     return (
       <div className="dashboard">
         <NavBar />
+        <Sort />
         <section className="main">
           <Sidebar />
-          <div className="questionList">
-            <h1>Unanswered Questions</h1>
-            <ul className="qMap">
-              {questions.map((question) => (
-                <li className="qLi" key={question.id}>
-                  {console.log(question)}
-                  <span className="questionHead">{question.question_body}</span>
-                  <br />
-                  {question.department_name}
-                  <br />
-                  <Moment format="YYYY/MM/DD">{question.created_date}</Moment>
-                  <br />
-                  {question.user_name}
-                  <Link to={`/post-answer/${question.id}`}>
-                    Answer Question
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <QuestionList handleQuestionLike={this.handleQuestionLike} handleDeleteQuestion={this.handleDeleteQuestion}
+          btnColors={this.state.btnColors} userID={user_id} questions={questions}/>
+         
         </section>
       </div>
     );
