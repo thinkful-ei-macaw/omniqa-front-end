832dca8fe7da35745c701e3c97d240d45d6bbd62
Alex Larrabee <nekotree556@gmail.com>
2020-06-11 14:34:40 -0700

    created questionItem component

---
+++ src/components/QuestionItem/QuestionItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Moment from "react-moment";
+
+function QuestionItem(props) {
+
+    let question = props.question;
+
+    return (
+        <li className="qLi" key={question.id}>
+            <span className="questionHead">{question.question_body}</span>
+            <br />
+            <br />
+            <span className="datePosted">
+                Posted on{" "}
+                <Moment format="YYYY/MM/DD">{question.created_date}</Moment>{" "}
+                by {question.user_name}
+            </span>
+            {props.answers.filter(answer => answer.question == question.id).map(answer => answer.answer_body)}
+            <br />
+            <br />
+            {/**update the button style color based on the question id. Call this handlequestion when the button is clicked*/}
+            {(question.author === props.userID) && <button onClick={() => props.handleDeleteQuestion(question.id, question.author)}>Delete</button>}
+            <button
+                style={{
+                    backgroundColor: props.btnColors[question.id]
+                        ? "#785380"
+                        : "white",
+                }}
+                onClick={() => props.handleQuestionLike(question.id)}
+                id="likeButton"
+            >
+                Like
+              </button>{" "}
+            <span className="hashtag">#{question.department_name}</span>
+            <br />
+            <br />
+        </li>
+    )
+
+
+}
+
+export default QuestionItem
--- src/components/QuestionList/QuestionList.js
+++ src/components/QuestionList/QuestionList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Moment from "react-moment";
+import QuestionItem from '../QuestionItem/QuestionItem';
 
 
 export default function QuestionList(props) {
@@ -10,35 +11,10 @@ export default function QuestionList(props) {
       <h1>Latest Questions</h1>
       <ul className="qMap">
         {props.questions.map((question) => (
-          <li className="qLi" key={question.id}>
-            <span className="questionHead">{question.question_body}</span>
-            <br />
-            <br />
-            <span className="datePosted">
-              Posted on{" "}
-              <Moment format="YYYY/MM/DD">{question.created_date}</Moment>{" "}
-                    by {question.user_name}
-            </span>
-            {props.answers.filter(answer => answer.question == question.id).map(answer => answer.answer_body)}
-            <br />
-            <br />
-            {/**update the button style color based on the question id. Call this handlequestion when the button is clicked*/}
-            {(question.author === props.userID) && <button onClick={() => props.handleDeleteQuestion(question.id, question.author)}>Delete</button>}
-            <button
-              style={{
-                backgroundColor: props.btnColors[question.id]
-                  ? "#785380"
-                  : "white",
-              }}
-              onClick={() => props.handleQuestionLike(question.id)}
-              id="likeButton"
-            >
-              Like
-                  </button>{" "}
-            <span className="hashtag">#{question.department_name}</span>
-            <br />
-            <br />
-          </li>
+          <QuestionItem question={question}
+            answers={props.answers}
+            handleQuestionLike={props.handleQuestionLike}
+            btnColors={props.btnColors} />
         ))}
       </ul>
     </div>
