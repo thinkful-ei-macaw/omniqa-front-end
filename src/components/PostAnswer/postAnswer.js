8cf33668fedb9e64bbe57dbf2295229f54448599
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-06-12 15:05:37 -0600

    started css on  answer post

--- src/components/PostAnswer/postAnswer.js
+++ src/components/PostAnswer/postAnswer.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import AnswerApiService from '../../Services/answers-service';
 import { Link } from 'react-router-dom';
+import './postAnswer.css';
 import QuestionContext from '../../Context/QuestionContext';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default class postAnswer extends Component {
   static contextType = QuestionContext;
@@ -15,23 +17,27 @@ export default class postAnswer extends Component {
 
   render() {
     return (
-      <div className='Answer'>
-        <form className='answer-form' onSubmit={(e) => this.handleSubmit(e)}>
-          <fieldset>
-            <br />
-            <label htmlFor='answer-label'>Answer</label>
-            <input
-              className='answer-input'
-              type='text'
-              name='answer_body'
-              id='answer-label'
-              placeholder='type your answer here'
-            />
-            <br />
-            <button type='submit'>Submit Answer</button>
-          </fieldset>
-        </form>
-        <Link to='/Dashboard'>Return to Dashboard</Link>
+      <div id='answerMain'>
+        <NavBar />
+        <div className='Answer'>
+          <form className='answer-form' onSubmit={(e) => this.handleSubmit(e)}>
+            <fieldset>
+              <legend>Your Answer</legend>
+              <br />
+              <label htmlFor='answer-label'>Answer</label>
+              <input
+                className='answer-input'
+                type='text'
+                name='answer_body'
+                id='answer-label'
+                placeholder='type your answer here'
+              />
+              <br />
+              <button type='submit'>Submit Answer</button>
+            </fieldset>
+          </form>
+          <Link to='/Dashboard'>Return to Dashboard</Link>
+        </div>
       </div>
     );
   }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000",
-  // API_ENDPOINT: 'https://murmuring-ridge-04034.herokuapp.com',
+  // API_ENDPOINT: 'http://localhost:8000',
+  API_ENDPOINT: 'https://murmuring-ridge-04034.herokuapp.com',
   TOKEN_KEY: 'omniqa-client-auth-token'
 };
