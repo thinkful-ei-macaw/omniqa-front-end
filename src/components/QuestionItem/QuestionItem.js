0d085ee3e307ad5f9782a82d6ef472d2e98ffa7b
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-06-12 13:28:34 -0600

    Pulling

--- src/components/QuestionItem/QuestionItem.js
+++ src/components/QuestionItem/QuestionItem.js
@@ -14,9 +14,6 @@ function QuestionItem(props) {
           <span className='questionHead' key='questionHead'>
             {question.question_body}
           </span>
-          {/* <button id='answerButton' onClick={() => props.handleAnswerQuestion(question.id)}>
-            Answer
-          </button> */}
           <Link to={`/post-answer/${question.id}`}>Answer</Link>
           <br />
           <br />
@@ -24,18 +21,20 @@ function QuestionItem(props) {
             Posted on <Moment format='YYYY/MM/DD'>{question.created_date}</Moment> by {question.user_name}
           </span>
           {props.answers.filter((answer) => answer.question == props.question.id).map((answer) => (
-            <div className='answer_body' key='answer'>
-              <div className='question_answer_body'>
-                <p className='answer-title'>Question Answer</p>
-                <div className='answer'>
-                  <p>{answer.answer_body}</p>
+            <section id='accordion__unchecked'>
+              <div className='answer_body' key='answer'>
+                <div className='question_answer_body'>
+                  <p className='answer-title'>Question Answer</p>
+                  <div className='answer'>
+                    <p>{answer.answer_body}</p>
+                  </div>
+                </div>
+                <div className='answer-user'>
+                  <p className='answer-title'>Answered By</p>
+                  {answer.user_name}
                 </div>
               </div>
-              <div className='answer-user'>
-                <p className='answer-title'>Answered By</p>
-                {answer.user_name}
-              </div>
-            </div>
+            </section>
           ))}
           <br />
           <br />
--- src/components/QuestionList/QuestionList.js
+++ src/components/QuestionList/QuestionList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Moment from 'react-moment';
 import QuestionItem from '../QuestionItem/QuestionItem';
-import TokenService from '../../Services/TokenService';
+import './QuestionList.css';
 
 export default function QuestionList(props) {
   return (
     <div className='questionList'>
-      <h1>Latest Questions</h1>
+      {/* <h1>Latest Questions</h1> Possibly add tags that can be turned off */}
       <ul className='qMap'>
         {console.log(props.userID)}
         {props.questions.map((question) => (
@@ -17,6 +17,7 @@ export default function QuestionList(props) {
             handleQuestionLike={props.handleQuestionLike}
             handleDeleteQuestion={props.handleDeleteQuestion}
             btnColors={props.btnColors}
+            handleAnswerQuestion={props.handleAnswerQuestion}
           />
         ))}
       </ul>
