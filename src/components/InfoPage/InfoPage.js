b1ab52cfd41f32602584d2bf838f90750572bc67
dasha akhten <dasha.akhten@gmail.com>
2020-06-12 00:41:06 -0700

    feature: info page

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./InfoPage.css";
 
 export class InfoPage extends Component {
   render() {
@@ -10,17 +11,42 @@ export class InfoPage extends Component {
           src={require("./logo.png")}
           alt="omni--logo"
         />{" "}
-        <h2>OMNI q&a has the answer to everything your team needs to know.</h2>
-        <h3>
+        <h2 className="info">
+          OMNI q&a has the answer to everything your team needs to know.
+        </h2>
+        <h3 className="info">
           Save time and increase productivity by directing team members to the
           questions they really need answers to!
         </h3>
-        <p>
-          Once a user has signed up, they can view the questions asked, filter
-          the questions by department name, and ask their own.
-          <br />
-          Don't forget, every time you're getting ready to commit changes, stand
-          up and yell out 'I'M PUSHING TO MASTER'
+        <p className="info">
+          <p>
+            Once a user has registered, they can view the questions asked,
+            filter the questions by department name, and ask their own.
+          </p>
+          <p>Users can 'like' questions to indicate their importance;</p>
+          <p>
+            Once signed up, users can also view the questions they asked.. even
+            compulsively checking for the answers
+          </p>
+          <p>But questions aint shit without answers! </p>
+          <p>
+            Users can answer questions they know the answer to, and all answers
+            will be displayed so everyone can easily judge them
+          </p>
+          <p>
+            Once you realise how dumb your question was, you can delete it! But
+            only if you are the one who asked it in the first place... we don't
+            want to appear rude
+          </p>
+          <p>
+            Don't forget, every time you're getting ready to commit changes,
+            stand up and yell out 'I'M PUSHING TO MASTER'
+          </p>
+          <p>
+            Demo username: dasha
+            <br />
+            password: Password123#
+          </p>
         </p>
         <Link to="/registration">
           {" "}
